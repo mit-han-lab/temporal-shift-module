1846ca95f5f00239945438b36a7455b1dd465bec
Tony Lin <to_tonylin@163.com>
2019-05-09 21:37:41 -0500

    Fix weight decay

--- scripts/train_tsn_kinetics_rgb_5f.sh
+++ scripts/train_tsn_kinetics_rgb_5f.sh
@@ -3,6 +3,6 @@
 
 python main.py kinetics RGB \
      --arch resnet50 --num_segments 5 \
-     --gd 20 --lr 0.02 --wd 5e-4 --lr_steps 20 40 --epochs 50 \
+     --gd 20 --lr 0.02 --wd 1e-4 --lr_steps 20 40 --epochs 50 \
      --batch-size 128 -j 16 --dropout 0.5 --consensus_type=avg --eval-freq=1 \
      --npb
